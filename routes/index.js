ac91d66e3b343a650f6f4eb6050b12de6c9d8252
YanlongLi <lansunlong@gmail.com>
2015-01-30 23:36:40 +0800

    DataCenter

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,34 @@ var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 var PUBLIC_DIRECTORY = "public/";
+var dataCenter = require('./DataCenter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 	fs.readFile(PUBLIC_DIRECTORY+"index.html", function(err, content){
-		console.log(content);
 		content.pipe(res);
 	});
+});
+
+router.get('/getApsByFloors', function(req, res, next) {
+	var floor = req.query.floor;
+	console.log(floors);
+	var apCenter = req.dataCenter.apCenter;
+	var result = apCenter.getApsByFloors(floor);
+	console.log("result:", result.length);
+	res.contentType('application/json');
+	res.send(JSON.stringify(result));
+	res.end();
+});
+router.get('/getApsByFloor', function(req, res, next) {
+	var iF = req.query.floor;
+	var apCenter = req.dataCenter.apCenter;
+	var result = apCenter.getApsByFloor(iF);
+	console.log("result:", result.length);
+	res.contentType('application/json');
+	res.send(JSON.stringify(result));
+	res.end();
 });
 
 module.exports = router;
