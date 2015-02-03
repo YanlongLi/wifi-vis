15184ebb91df57e843021d254877ba862b5658a7
YanlongLi <lansunlong@sjtu.edu.cn>
2015-02-03 10:15:01 +0800

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/getApsByFloor', function(req, res, next) {
 	var iF = req.query.floor;
 	var apCenter = req.dataCenter.apCenter;
 	var result = apCenter.getApsByFloor(iF);
-	console.log("getApsByFloor:", floor);
+	console.log("getApsByFloor:", iF);
 	console.log("result:", result.length);
 	res.contentType('application/json');
 	res.send(JSON.stringify(result));
