80d737b72c56dde30edb13322bc34fd3db9d8dc6
YanlongLi <lansunlong@sjtu.edu.cn>
2015-02-03 20:30:31 +0800

    __initdb directory

---
+++ _initdb/init.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var path = require('path');
+var csv = require('fast-csv');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/wifi-vis');
+
+var aps = db.get('aps_collection');
+var records = db.get('records_collecton');
+
+
+var AP_FILE_PATH = path.join(__dirname, '../public/data/APS.csv');
+var apStream = fs.createReadStream(AP_FILE_PATH);
+batchInsert(apStream, aps, function(c){console.log("insert aps done:", c)});
+
+var rcFileName = ["2013-09-01.csv", "2013-09-02.csv", "2013-09-03.csv", "2013-09-04.csv",
+"2013-09-05.csv", "2013-09-06.csv", "2013-09-07.csv", "2013-09-08.csv",
+"2013-09-09.csv", "2013-09-10.csv", "2013-09-11.csv", "2013-09-12.csv",
+"2013-09-13.csv", "2013-09-14.csv", "2013-09-15.csv", "2013-09-16.csv",
+"2013-09-17.csv", "2013-09-18.csv", "2013-09-19.csv", "2013-09-20.csv",
+"2013-09-21.csv", "2013-09-22.csv", "2013-09-23.csv", "2013-09-24.csv",
+"2013-09-25.csv", "2013-09-26.csv", "2013-09-27.csv", "2013-09-28.csv",
+"2013-09-29.csv", "2013-09-30.csv"];
+
+rcFileName.forEach(function(fname){
+	var csvPath = path.join(__dirname,'../public/data/September/', fname);
+	var rcStream = fs.createReadStream(csvPath);
+	batchInsert(rcStream,records, function(c){console.log(csvPath,"done:",c)});
+});
+
+
+function batchInsert(stream, collection, next){
+	var count = 0;
+	var apCsv = csv({headers:true}).on('data',function(ap, err){
+		if(err) console.log(err);
+		else{
+			collection.insert(ap, function(err,doc){ if(err) throw err; });
+			count++;
+		}
+	}).on('end',function(err){
+		if(next) next(count);
+	});
+	stream.pipe(apCsv);
+}
+
