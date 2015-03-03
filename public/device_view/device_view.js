c669d3dbf9271cab7c6a1a217e0b681d1784014b
J-Miao <j.miao92@gmail.com>
2015-03-03 13:55:55 +0800

    new device view

--- public/device_view/device_view.js
+++ public/device_view/device_view.js
@@ -1,5 +1,7 @@
 var db = new WFV_DB(new Date(2013,08,02), new Date(2013,08,03));
 var access_data;
+var apFloorMappings = {};
+var apConnCnt = {};
 
 function get_access_data(mac, dates){
   var access_data = [];
@@ -9,6 +11,11 @@ function get_access_data(mac, dates){
     var next_date = new Date(date).setDate(date.getDate() +1);
     var path = db.path_by_mac(mac, date, next_date);
     console.log(path)
+    path.forEach(function(d) {
+      if (!(d.apid in apFloorMappings)) {
+        apFloorMappings[d.apid] = d.ap.floor;
+      }
+    });
     path = path.map(function(r,i){
       if(i == 0 || r.apid != path[i-1].apid) return r;
       return null;
@@ -28,7 +35,8 @@ function get_access_data(mac, dates){
 
 db.init(function(){
   var dates = [new Date(2013,08,02)];
-  access_data = get_access_data("ac02bf41ce", dates)
+  //access_data = get_access_data("ac02bf41ce", dates)
+  access_data = get_access_data("5dfab8ca93", dates)
   // console.log(access_data);
   device_view(access_data);
 });
@@ -68,9 +76,9 @@ function device_view(access_data){
   var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom")
-      .tickFormat(function(d) {
-        return xformatDate(new Date(d - 8*60*60*1000))
-      })
+      // .tickFormat(function(d) {
+      //   return xformatDate(new Date(d - 8*60*60*1000))
+      // })
       .ticks(5);
 
   var yAxis = d3.svg.axis()
@@ -93,12 +101,13 @@ function device_view(access_data){
  
 
 
-  x.domain([0, 2*60*60*1000]);
-  y.domain(access_data[0].line.map(function(d) {
-    // console.log(d[0].date_time.to_date_str()); 
-    // console.log(d.apid)  
-    return d.apid;
-  }));
+  //x.domain([0, 2*60*60*1000]);
+  x.domain([0, 24*60*60*1000]);
+  // y.domain(access_data[0].line.map(function(d) {
+  //   // console.log(d[0].date_time.to_date_str()); 
+  //   // console.log(d.apid)  
+  //   return d.apid;
+  // }));
   // var yDomain = d3.extent(access_data, function(d) {return d[0].date})
   // var domainMin = new Date(new Date(yDomain[0]).getTime()-86400000)
   // var domainMax = new Date(new Date(yDomain[1]).getTime()+86400000)
@@ -107,6 +116,8 @@ function device_view(access_data){
   // y.domain([new Date(domainMin), new Date(domainMax)])
   // console.log(y.domain())
   // console.log(y("2013-09-04"))
+
+  console.log(apFloorMappings);
 
   var line = d3.svg.line()
       .x(function(d){
@@ -132,18 +143,115 @@ function device_view(access_data){
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
-  svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis);
+  // svg.append("g")
+  //   .attr("class", "y axis")
+  //   .call(yAxis);
 
-  svg.selectAll(".line")
-    .data(access_data[0].lines)
-    .enter().append("path")
-    .attr("class", "line")
-    .attr("d", line);
+  // svg.selectAll(".line")
+  //   .data(access_data[0].lines)
+  //   .enter().append("path")
+  //   .attr("class", "line")
+  //   .attr("d", line);
 
+  console.log(access_data[0].lines);
+  var tempDataset = access_data[0].lines.filter(function(d) {
+      var h0 = d[0].date_time.getHours();
+      var m0 = d[0].date_time.getMinutes();
+      var s0 = d[0].date_time.getSeconds();
+      var h1 = d[1].date_time.getHours();
+      var m1 = d[1].date_time.getMinutes();
+      var s1 = d[1].date_time.getSeconds();
+      
+      var ml0 = (h0*3600+m0*60+s0)*1000, ml1 = (h1*3600+m1*60+s1)*1000;
+      return true;
+      //return x(ml1) - x(ml0) > 1.0;
+  });
+  var dataset = [], cnt = 0;
+  dataset.push(tempDataset[0]);
+  for (var i = 1; i < tempDataset.length; i++) {
+    var cur = tempDataset[i];
+    var h0 = dataset[cnt][1].date_time.getHours();
+    var m0 = dataset[cnt][1].date_time.getMinutes();
+    var s0 = dataset[cnt][1].date_time.getSeconds();
+    var h1 = cur[0].date_time.getHours();
+    var m1 = cur[0].date_time.getMinutes();
+    var s1 = cur[0].date_time.getSeconds();
+    var ml0 = (h0*3600+m0*60+s0)*1000, ml1 = (h1*3600+m1*60+s1)*1000;
+    if (x(ml1) - x(ml0) > 5.0 || cur[0].apid != dataset[cnt][1].apid) {
+      dataset.push(cur);
+      cnt++;
+    }
+    else {
+      dataset[cnt][1] = cur[1];
+    }
+  }
+
+  console.log(dataset);
+  dataset.forEach(function(d) {
+    if (!(d[0].apid in apConnCnt)) {
+        apConnCnt[d[0].apid] = 1;
+    }
+    else {
+       apConnCnt[d[0].apid] ++; 
+    }
+  });
+
+  console.log(apConnCnt);
+  y.domain(Object.keys(apFloorMappings).filter(function(d) {
+    return apConnCnt[d] > 0;
+  }));
+
+  svg.selectAll(".apTag")
+    .data(y.domain())
+    .enter().append("text")
+    .attr("class", "apTag")
+    .attr("x", 0)
+    .attr("y", function(d) {
+      return y(d)+y.rangeBand()/2.0 - 2.5;
+    })
+    .text(function(d) {
+      return d;
+    });
+
+  svg.selectAll(".rect")
+    .data(dataset)
+    .enter().append("rect")
+    .attr("class", "rect")
+    .attr("x", function(d){
+        var h = d[0].date_time.getHours();
+        var m = d[0].date_time.getMinutes();
+        var s = d[0].date_time.getSeconds();
+        var ml = (h*3600+m*60+s)*1000;
+        return x(ml);
+      })
+    .attr("y", function(d){
+        // var r = y(d.date_time.to_date_str());
+        // console.log(d.date_time.to_date_str())
+        // console.log(r)
+        // r = y(new Date(d.date))
+        // console.log(r, d.date, y.domain())
+        // return r;
+        return y(d[0].apid)+y.rangeBand()/2.0 - 2.5;
+      })
+    .attr("width", function(d){
+        var h0 = d[0].date_time.getHours();
+        var m0 = d[0].date_time.getMinutes();
+        var s0 = d[0].date_time.getSeconds();
+        var h1 = d[1].date_time.getHours();
+        var m1 = d[1].date_time.getMinutes();
+        var s1 = d[1].date_time.getSeconds();
+        
+        var ml0 = (h0*3600+m0*60+s0)*1000, ml1 = (h1*3600+m1*60+s1)*1000;
+        return x(ml1) - x(ml0);
+    })
+    .attr("height", 5)
+    ;
+
+
+  console.log(Array.prototype.concat.apply([], dataset));
   svg.selectAll(".dot")
-    .data(Array.prototype.concat.apply([],access_data[0].line))
+    //.data(Array.prototype.concat.apply([],access_data[0].line))
+    .data(Array.prototype.concat.apply([], dataset))
     .enter().append("circle")
     .attr("class", "dot")
     .attr("r", 2.5)
