56f738230805c71a82cc5f1349c7a942eeda4310
YanlongLi <lansunlong@gmail.com>
2015-03-28 01:10:16 +0800

    --

--- public/main.js
+++ public/main.js
@@ -1,6 +1,23 @@
+var params = (function(){
+	var queryString = window.location.search.substring(1);
+	if(queryString.length < 16) return {}
+	queryString = queryString.slice(0, queryString.length-1);
+	var params = {};
+	var keyValue = queryString.split("&").map(function(d){return d.split("=")}).forEach(function(d){
+		params[d[0]] = d[1];
+	});
+	var format = d3.time.format("%Y%m%d");
+	for(attr in params){
+		params[attr] = format.parse(params[attr]);
+	}
+	console.log("params", params.from);
+	console.log("params", params.to);
+	return params;
+})();
+
 var currentZIndex = 100;
-var timeFrom = new Date(2013,8,2),
-		timeTo   = new Date(2013,8,3);
+var timeFrom = params.from || new Date(2013,8,2),
+		timeTo   = params.to || new Date(2013,8,3);
 var db = new WFV_DB(timeFrom, timeTo);
 
 var apLst = [], apMap = d3.map(), macMap;
@@ -10,6 +27,7 @@ var tracer = new RecordTracer();
 var db_tl = new WFV_TL_DATA();
 
 var floor_bar = WFV.FloorBar([timeFrom, timeTo]);
+var floor_bar_floor_aps = WFV.FloorBarFloorAps([timeFrom, timeTo]);
 
 var floorDetail, floorsNav, timeline;
 var apGraph, deviceGraph, controllerView;
@@ -78,19 +96,21 @@ function init(){
 	
 	floorDetail = WFV.FloorDetail();
 	timeline = WFV.Timeline([timeFrom, timeTo]);
-	// apGraph
-	apGraph = WifiVis.ApGraph();
-	apGraph.init();
-	apGraph.draw(); 
-	// deviceGrpah
-	deviceGraph = WifiVis.DeviceGraph();
-	deviceGraph.init();
-
-	deviceView = WFV.DeviceView();
-	// deviceView.draw();
-	apView = WFV.ApView();
-	//
-	deviceStats = WFV.DeviceStats();
+/*
+ *   // apGraph
+ *   apGraph = WifiVis.ApGraph();
+ *   apGraph.init();
+ *   apGraph.draw(); 
+ *   // deviceGrpah
+ *   deviceGraph = WifiVis.DeviceGraph();
+ *   deviceGraph.init();
+ * 
+ *   deviceView = WFV.DeviceView();
+ *   // deviceView.draw();
+ *   apView = WFV.ApView();
+ *   //
+ *   deviceStats = WFV.DeviceStats();
+ */
 
 	//
 	EventManager.floorChange(1);
